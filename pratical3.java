0c7494a38f05003da10fd9b7807bd3c6aed75b9f
Akashmishra55 <112937737+Akashmishra55@users.noreply.github.com>
2022-09-08 21:06:46 +0530

    Add files via upload

---
+++ pratical3.java
@@ -0,0 +1,16 @@
+public class akash
+{
+int a=25;    //Intance variable
+static double b=20;   //static variable
+public static void main(String[] args)
+{
+boolean c=true;    //local variable 
+
+               akash s=new akash();
+               
+               System.out.println(s.a);
+               System.out.println(b);
+               System.out.println(c);
+
+        }
+}
