35464b3c88bf5df55a322bb1ac59de8906e1a5a0
Akashmishra55 <112937737+Akashmishra55@users.noreply.github.com>
2022-09-08 18:18:13 +0530

    Add files via upload

---
+++ pratical2.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Akash
+{
+public static void main(String[] args)
+{
+
+int a,b,c,k;
+System.out.print("Enter any three number: ");
+Scanner  t=new Scanner(System.in);
+a=t.nextInt();
+b=t.nextint();
+c=t.nextInt();
+k=a*b*c;
+System.out.print("multiplication of three number: "+k);
+}
+}
+
