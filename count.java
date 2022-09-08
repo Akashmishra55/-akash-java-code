9c81ba702e1beee66e44071a20d484a6d99a8325
Akashmishra55 <112937737+Akashmishra55@users.noreply.github.com>
2022-09-08 20:57:52 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,14 @@
+//A fruit seller had some apples. He sells 40% apples and still has 420 apples. Originally, he had:how many apples ??
+
+
+class SellApple 
+{
+public static void main(String[] args)  //usning main class
+{
+int a = 420;  // insert value of a
+int b = 100;  //  insert value of b
+int x = a*b/60;   // multiplication of variable a and b
+System.out.println(" he have " + x + " apple");  //printing the output
+}
+}
+ 
