1bd321f8bec4f62058662bddd8e460ebf63b4daa
Akashmishra55 <112937737+Akashmishra55@users.noreply.github.com>
2022-09-15 15:41:28 +0530

    Add files via upload

---
+++ three.java
@@ -0,0 +1,16 @@
+/**1.  Write a java program to find out below problem, in a small company, 
+the average salary of three employees is Rs10000 per week.
+If one employee earns Rs11000 and other earns Rs5000, how much will the third employee earn?**/
+
+class Three
+{
+static public void main(String args[])
+{
+int a=10000;
+int b=11000;
+int c=5000;
+int z=a-b-c;
+System.out.print("salary of the third employee"+" "+z);
+}
+}
+
