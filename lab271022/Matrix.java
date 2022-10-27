38fc1ea065222abbb8a5cdce3ea37106a2d494a4
Akashmishra55 <112937737+Akashmishra55@users.noreply.github.com>
2022-10-27 12:49:06 +0530

    Add files via upload

--- lab271022/Matrix.java
+++ lab271022/Matrix.java
@@ -1,27 +1,24 @@
 package lab271022;
 
 import java.io.*;
-
-
 //wap to print 4*4 matrix
 public class Matrix {
-	@SuppressWarnings("deprecation")
 	public static void main(String args[])throws Exception
 	{
 		int ar[][]=new int[4][4];
 		DataInputStream dis= new DataInputStream(System.in);
-		System.out.println("Enter the Elements for 4*4 array:");
-		for(int i=0;i<=4;i++)
+		System.out.println("Enter  Elements for 4*4  array:");
+		for(int i=0;i<=3;i++)
 		{
-			for(int j=0;j<=3;j++)
+			for(int j=0;j<=2;j++)
 			{
 				ar[i][j]=Integer.parseInt(dis.readLine());
 				
 			}
 			System.out.println("Array Elements are:");
-			for(i=0;i<=4;i++)
+			for(i=0;i<=3;i++)
 			{
-				for(int j = 0;j<=3;j++)
+				for( int j = 0;j<=2;j++)
 				{
 					System.out.print(ar[i][j]+"");		
 				}
